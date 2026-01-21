27ed9dc44d8bdf043bb6ff69f6645cd1678cbcfe
Patsaraporn Phadungsil <68010852@kmitl.ac.th>
2026-01-21 16:58:36 +0700

    Update persons.ts

--- src/practicals/p07/persons.ts
+++ src/practicals/p07/persons.ts
@@ -1,11 +1,45 @@
+export{}
+interface Person {
+  firstName?: string;
+  lastName?: string;
+  age?: number;
+}
 interface Person {
   firstName?: string;
   lastName?: string;
   age?: number;
 }
 
-function sortPersons(persons: Person[]): Person[] {}
+function sortPersons(persons: Person[]): Person[] {
 
+  const validPersons = persons.filter(person => {
+
+    if (typeof person.age !== 'number' || person.age < 0 || person.age > 120) {
+      return false;
+    }
+    
+
+    if (!person.firstName || person.firstName.trim() === "") {
+      return false;
+    }
+
+    return true;
+  });
+
+  return validPersons.sort((a, b) => {
+
+    if (a.age !== b.age) {
+      return a.age - b.age;
+    }
+
+    if (a.firstName !== b.firstName) {
+      return a.firstName.localeCompare(b.firstName);
+    }
+    
+ 
+    return (a.lastName || '').localeCompare(b.lastName || '');
+  });
+}
 
 const persons: Person[] = [
   { firstName: "Somchai", lastName: "Jaidee", age: 30 },
@@ -16,3 +50,7 @@ const persons: Person[] = [
   { firstName: "Jane", lastName: "Smith", age: -5 },
   { lastName: "NoName", age: 40 }
 ];
+
+console.log(sortPersons(persons));
+
+
