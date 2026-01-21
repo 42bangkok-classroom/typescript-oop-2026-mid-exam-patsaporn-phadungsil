a2343bc05e4aabbd27adf8087165479db3ca4783
Patsaraporn Phadungsil <68010852@kmitl.ac.th>
2026-01-21 14:57:11 +0700

    Update grading.ts

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,23 @@
 // Write your code below
+const grade = +process.argv[2]
+const point = Number(grade)
+if(Number.isNaN(point)){
+    console.log("Invalid Input");
+}else if (point < 0 || point > 100){
+    console.log("Invalid Input");
+}else{
+  if(grade >= 80){
+    console.log("Grade is A");
+  } else if(grade >= 70){
+    console.log("Grade is B");
+  } else if(grade >= 60){
+    console.log("Grade is C");
+  } else if(grade >= 50){
+    console.log("Grade is D");
+  } else{
+    console.log("Grade is F");
+}
+}
+
+
+
