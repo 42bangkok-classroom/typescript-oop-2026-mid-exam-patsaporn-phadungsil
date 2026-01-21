9bbedb4b8be11e03f175d7e3669ee459f31622a7
Patsaraporn Phadungsil <68010852@kmitl.ac.th>
2026-01-21 16:41:47 +0700

    Update shorten-comment.ts

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -2,4 +2,10 @@ const comment = "TypeScript is a strongly typed programming language"
 
 function shortenComment(comment: string): string {
   // Write your code below
+const word = comment.split("");
+const filterWord = word.filter(word => word.length >= 5 && word.length <= 10);
+if(filterWord.length === 0){
+  return ""; }
+  return filterWord.join("");
 }
+console.log(shortenComment(comment));
