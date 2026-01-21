a60fc395da358015c761fbff23f66133cb3773d4
Patsaraporn Phadungsil <68010852@kmitl.ac.th>
2026-01-21 16:34:08 +0700

    commmit

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,20 @@
 // Write your code below
+const i: number =+ process.argv[2];
+
+if(Number.isNaN(i)){
+    console.log("Invalid input");
+}else if (i <= 0){
+    console.log("Invalid input");
+for(let i = 1;i <= i ;i++){
+    if(i % 15 == 0 ){
+        console.log("FizzBuzz");
+    }else if(i % 3 == 0){
+        console.log("Fizz");
+    }else if(i % 5 == 0){
+        console.log("Buzz");
+    }else{
+        console.log(i);
+    }
+}
+    
+}
--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,23 @@
 // Write your code below
+
+const py = +process.argv[2];
+
+if(Number.isNaN(py) || py <= 0){
+  process.exit()
+}
+function printPattern(rows: number): void {
+for(let i = 1 ; i <= py; i++){
+    let result = "";
+    for(let j = 1;j <= i - 1; j++){
+      result +="";
+
+    }
+    for (let j = i; j >= 1; j--){
+    
+    result+= j;
+    }
+    console.log(result);
+   
+  }
+
+}
