1601d00b15bde5b62eb7c9b24572fa2f4a97c7b8
Patsaraporn Phadungsil <68010852@kmitl.ac.th>
2026-01-21 16:55:11 +0700

    Update unique.ts

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,6 +1,29 @@
 function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
   // Write your code below
+
+const combined = [...arr1, ...arr2];
+  
+  
+  return combined.filter((num, index, self) => 
+    self.indexOf(num) === self.lastIndexOf(num)
+  );
+
+
 }
+
 
 const arr1 = [1, 2, 3, 4];
 const arr2 = [3, 4, 5, 6];
+
+console.log(getUniqueNumbers(arr1, arr2));  
+
+
+
+
+
+
+
+
+
+
+  
